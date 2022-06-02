b0e346cafa0143f524acfd6429d15b8c5accbc6c
Marcelo Bianco Queiroz <marcelo.bianco@perplan.intra>
2022-06-02 13:29:17 -0300

    Apagando

--- src/js/events.js
+++
@@ -1,8 +0,0 @@
-const menuLi = document.getElementById("li-menu");
-const dropdown = document.getElementById("dropdown");
-const menuIcon = document.getElementsByClassName("menu-icon")[0];
-
-menuLi.onclick = () => { 
-  dropdown.classList.toggle('dropdown-click');
-  menuIcon?.classList.toggle('menu-icon-click');
-}
